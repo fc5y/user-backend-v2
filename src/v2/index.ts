5f63b785223538378b0a2dc0c7320cb124a62027
Kien Nguyen <kc97ble@gmail.com>
2021-05-13 18:27:46 +0800

    feat: Add GET /api/v2

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -3,6 +3,26 @@ import { getCurrentTimestamp } from './utils';
 
 const router = Router();
 
+router.get('/', (req: Request, res: Response) => {
+  res.json({
+    error: 0,
+    error_msg: '',
+    // Format: https://docs.npmjs.com/cli/v7/configuring-npm/package-json
+    data: {
+      name: 'user-backend-v2',
+      version: '2.0.0-test.1',
+      description: 'User Backend v2',
+      homepage: '',
+      contributors: [
+        {
+          name: 'Kien Nguyen',
+          email: 'kc97ble@gmail.com',
+        },
+      ],
+    },
+  });
+});
+
 router.get('/timestamp', (req: Request, res: Response) => {
   res.json({
     data: {
