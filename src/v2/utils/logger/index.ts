07cba2477037eeb26d5595ce7b8b3bd54bf09cde
Anh Cao Van The <caovantheanh@gmail.com>
2021-07-04 12:42:01 +0700

    feat: add logger

---
+++ src/v2/utils/logger/index.ts
@@ -0,0 +1,16 @@
+import { createLogger, format, transports } from 'winston';
+const { combine, printf, colorize } = format;
+
+const myFormat = printf(({ level, message }) => {
+  return `[${level}] ${message}`;
+});
+const logger = createLogger({
+  'format': combine(
+    colorize(),
+    myFormat,
+  ),
+  'transports': [
+    new transports.Console(),
+  ]
+});
+export = logger;
