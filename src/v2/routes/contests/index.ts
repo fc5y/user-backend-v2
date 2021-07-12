f41ab00407b1bc7fd5ffb8e0a629fcc0971d6598
Kien Nguyen <kc97ble@gmail.com>
2021-07-12 21:44:19 +0800

    feat: Add GET /api/v2/contests/:contest_name

--- src/v2/routes/contests/index.ts
+++ src/v2/routes/contests/index.ts
@@ -109,8 +109,73 @@ async function createContest(req: Request, res: Response, next: NextFunction) {
 
 // #endregion
 
+// #region GET /api/v2/contests/:contest_name
+
+type GetContestParams = {
+  contest_name: string;
+};
+
+const getContestParamsSchema: JSONSchemaType<GetContestParams> = {
+  type: 'object',
+  required: ['contest_name'],
+  properties: {
+    contest_name: { type: 'string' },
+  },
+};
+
+async function getContest(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { contest_name } = assertWithSchema(req.params, getContestParamsSchema);
+    const { error, error_msg, data } = await db.contests.getContests({
+      offset: 0,
+      limit: 1,
+      has_total: false,
+      contest_name,
+    });
+
+    if (error || !data) {
+      throw new GeneralError({
+        error: ERROR_CODE.DATABASE_GATEWAY_ERROR,
+        error_msg: 'Received non-zero code from Database Gateway when getting contest',
+        data: { response: { error, error_msg, data } },
+      });
+    }
+
+    const contest = data.items[0];
+
+    if (!contest) {
+      throw new GeneralError({
+        error: ERROR_CODE.CONTEST_NOT_FOUND,
+        error_msg: 'Contest not found',
+        data: { contest_name },
+      });
+    }
+
+    res.json({
+      error: 0,
+      error_msg: 'Contest',
+      data: {
+        contest: {
+          name: contest.contest_name,
+          title: contest.contest_title,
+          start_time: contest.start_time,
+          duration: contest.duration,
+          total_participations: await getTotalPartitipationsInContest(contest.id),
+          can_enter: contest.can_enter,
+          materials: formatMaterials(contest.materials),
+        },
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+// #endregion
+
 const router = Router(); // /api/v2/contests
 router.get('/', getAllContests);
 router.post('/create', mustBeAdmin, createContest);
+router.get('/:contest_name', getContest);
 
 export default router;
