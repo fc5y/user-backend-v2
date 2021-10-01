cf40b1fdff2bee8c3566c4530792f393a243dc90
Cong Nguyen <congcongcong6e@gmail.com>
2021-10-01 14:32:23 +0700

    done check regex

--- src/v2/routes/auth/utils.ts
+++ src/v2/routes/auth/utils.ts
@@ -1,6 +1,8 @@
 import { ERROR_CODE, GeneralError } from '../../utils/common-errors';
 
 const REGEX_EMAIL_LOOSE = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const REGEX_USERNAME_LOOSE = /^[a-zA-Z0-9_-]{3,16}$/;
+const REGEX_PASSWORD_LOOSE = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
 export function assertEmail(email: string) {
   if (!REGEX_EMAIL_LOOSE.test(email)) {
@@ -14,11 +16,23 @@ export function assertEmail(email: string) {
 }
 
 export function assertUsername(username: string) {
-  // TODO: add validation
+  if (!REGEX_USERNAME_LOOSE.test(username)) {
+    throw new GeneralError({
+      error: ERROR_CODE.INVALID_USERNAME,
+      error_msg: 'Invalid username',
+      data: { username },
+    });
+  }
   return username;
 }
 
 export function assertPassword(password: string) {
-  // TODO: add validation
+  if (!REGEX_PASSWORD_LOOSE.test(password)) {
+    throw new GeneralError({
+      error: ERROR_CODE.INVALID_PASSWORD,
+      error_msg: 'Invalid password',
+      data: { password },
+    });
+  }
   return password;
 }
