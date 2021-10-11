38a60b80f5645d4aeff2c8c85dea80f6acd3cd33
minhhuylqd <minhhuy2810@gmail.com>
2021-10-12 05:38:49 +0700

    Add updateUserPasswordOrThrow and getUserWithEmail

--- src/v2/utils/database-gateway-wrapper/components/users.ts
+++ src/v2/utils/database-gateway-wrapper/components/users.ts
@@ -41,3 +41,41 @@ export async function createUserOrThrow(params: {
     });
   }
 }
+
+export async function updateUserPasswordOrThrow(user_id: number, new_password: string) {
+  const updateResponse = await db.users.updateUser({
+    where: {
+      user_id: user_id,
+    },
+    values: {
+      password: new_password,
+    },
+  });
+  if (updateResponse.error) {
+    throw new GeneralError({
+      error: ERROR_CODE.DATABASE_GATEWAY_ERROR,
+      error_msg: 'Received non-zero code from Database Gateway when updating user password',
+      data: { response: updateResponse },
+    });
+  }
+}
+
+export async function getUserWithEmail(email: string) {
+  const { error, error_msg, data } = await db.users.getUsers({ email, offset: 0, limit: 1 });
+  if (error || !data) {
+    throw new GeneralError({
+      error: ERROR_CODE.DATABASE_GATEWAY_ERROR,
+      error_msg: 'Received non-zero code from Database Gateway when getting contests',
+      data: { response: { error, error_msg, data } },
+    });
+  }
+  const user = data.items[0];
+  if (!user) {
+    throw new GeneralError({
+      error: ERROR_CODE.USER_NOT_FOUND,
+      error_msg: 'User not found',
+      data: { email },
+    });
+  }
+  return user;
+}
